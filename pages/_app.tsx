4fd34308df0bb3c44472140443ecda3ca9e847f3
lumenwrites <lumenwrites@gmail.com>
2022-05-24 01:01:46 +0100

    Basic styles.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/style.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
