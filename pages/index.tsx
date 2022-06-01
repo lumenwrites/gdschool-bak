5fa61cfccc5ea562461c9d66e2148136dc836f22
lumenwrites <lumenwrites@gmail.com>
2022-06-01 03:47:01 +0100

    Update.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Login from 'components/Users/Login'
 export default function Home({ lessons }) {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello!</h1>
       <Login />
     </div>
   )
