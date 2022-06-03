ff105c973e5779713771eb95409188b991952c41
lumenwrites <lumenwrites@gmail.com>
2022-06-03 20:21:06 +0100

    Fetching the course and running a script on it.

---
+++ backend/json/processContent.mjs
@@ -0,0 +1,5 @@
+import { processCourses } from "./processCourses.mjs"
+
+console.log("[processContent] Generating json...")
+await processCourses()
+
---
+++ backend/json/processCourses.mjs
@@ -0,0 +1,18 @@
+import { join } from 'path'
+import { readFileSync, readdirSync, writeFileSync } from 'fs'
+import { lstatSync, existsSync } from 'fs'
+
+const coursesDir = './courses'
+const jsonDir = join(process.cwd(), './backend/json/courses')
+
+
+export async function processCourses() {
+  console.log('[processCourses] Generating course json...')
+
+  for (const courseDirName of readdirSync(coursesDir)) {
+    const courseDirPath = join(coursesDir, courseDirName)
+    if (!lstatSync(courseDirPath).isDirectory()) continue // ignore .DS_Store
+    console.log("Processing course:", courseDirName)
+    console.log('[processCourses] Success! Markdown courses converted to json.')
+  }
+}
